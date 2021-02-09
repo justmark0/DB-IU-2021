5f757bfc3f3d475b91a6ca66b1184d1f9b09ec61
Mark Nicholson <wmark.nicholsonu@gmail.com>
2021-02-09 14:24:38 +0300

    week3

--- lab3/Task1.sql
+++ lab3/Task1.sql
@@ -1,6 +1,5 @@
 -- subTASK 1
--- Relational algebra
-tau_{country_id}(country)
+-- Relational algebra: tau_{country_id}(country)
 -- TABLE:
  CREATE TABLE "contry" (
   "id" SERIAL PRIMARY KEY,
@@ -12,8 +11,7 @@ Select country.id from contry
 
     
 -- subTASK 2
--- Relational algebra:
-/* LIKE A% */ 
+-- Relational algebra: sigma_{ LIKE 'A%'}(city)
 --(reference for relational algebra: https://stackoverflow.com/questions/9875319/relational-algebra-check-for-error)
 -- Table: city will have one to many correspondance to table address. Required fields you can see from the query
 --Query
@@ -21,8 +19,7 @@ SELECT address FROM city WHERE address LIKE 'A%';
 
 
 --subTASK 3
---Relational algebra: 
-/* sigma_{first_name, last_name, city}(customers) */
+--Relational algebra: sigma_{first_name, last_name, city}(customers)
 -- Table
  CREATE TABLE "customers" (
   "id" SERIAL PRIMARY KEY,
@@ -35,10 +32,13 @@ Select first_name, last_name, city from customers;
 
 
 -- subTASK 4
--- Relational algebra:
-
+-- Relational algebra: sigma_{idc:idc in sigma_{amount, idp: idp=idc and amount>11}(payments)}(customers)
 -- Table: customer will have one to many correspondance to table payments. Required fields you can see from the query
 -- Query
 Select * from customer AS cust WHERE (Select amount from payments WHERE customer_id = cust.id AND amount>11);
 
+-- subTASK 5
+-- Relational algebra:  sigma_{id1,n1: sigma_{id2, n2: id2!=id1 and n1=n2}}
+-- Query:
+Select * from cusomers AS cust WHERE (Select first_name, id from customers WHERE first_name=cust.first_anme AND id!=cust.id)
 
